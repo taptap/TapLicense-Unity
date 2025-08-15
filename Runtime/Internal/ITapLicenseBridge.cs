d5fd715bd7a6b4a2a936984c87cbe038a58292e8
ci-gitlab <tds-developer@xd.com>
2025-08-15 21:32:26 +0800

    feat:update upm

---
+++ Runtime/Internal/ITapLicenseBridge.cs
@@ -0,0 +1,22 @@
+﻿
+namespace TapTap.License.Internal {
+    public interface ITapLicenseBridge
+    {
+        void SetDLCCallback(ITapDlcCallback callback);
+
+        void QueryDLC(string[] skus);
+
+        void SetDLCCallback(ITapDlcCallback callback, bool checkOnce, string publicKey);
+
+        void Check(bool force = false);
+
+        void SetLicencesCallback(ITapLicenseCallback callback);
+
+        void PurchaseDLC(string sku);
+
+        void SetTestEnvironment(bool isTest);
+
+
+
+    }
+}
